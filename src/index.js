2f177a7f401a701fa2bf2505cb6bedee44688726
ciro golden <cgoldennlo@gmail.com>
2019-02-06 13:47:06 -0800

    Create initial webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./module-a";
 // Webpack will, by default, look for a entry file
 // at this location (i.e. /src/index.js)
-console.log("Hello, World!");
+
+// You can supply webpack with the --mode=development option:
+// webpack --mode=development
+
+// You can tell webpack to auto-bundle your code with:
+// webpack --watch
+console.log("Hello, Multiverse!");
