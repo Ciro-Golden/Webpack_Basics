c89f85af93be38a723852ff8a7c21653bd9d1d42
ciro golden <cgoldennlo@gmail.com>
2019-02-06 14:27:11 -0800

    Configure output bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
-  //   entry: "./src/client.js"
+  //   entry: "./src/client.js"  >>> Single
+  // entry: ["./src/client.js", "./src/server.js"] >>> Multiple
   entry: {
     client: "./src/client.js",
     server: "./src/server.js"
+  },
+  output: {
+    // The "path" propert is used to specify the location
+    // where our bundle files are output. By default, it
+    // is path.join(__dirname, "dist")
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
   }
 };
